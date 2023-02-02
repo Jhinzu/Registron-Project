437c0543044a86b0864a249ee899695b132a7b03
Rani Derradj <raniderradj2@gmail.com>
2023-02-02 11:11:01 +0100

    add tab

---
+++ registron.js
@@ -0,0 +1,15 @@
+const c1 = document.querySelector("#selectionC1");
+const a1 = document.querySelector("#anneau1");
+
+console.log(c1);
+// console.log(a1);
+
+const selectValeurs = () => 
+{
+    for (i=1;i<10;i++)
+    {
+        console.log(c1[i]);
+        if (c1[i] = )
+    }
+}
+
